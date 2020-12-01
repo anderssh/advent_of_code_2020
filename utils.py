960e083a40b26cfc2f638e18c9a50cf51169c109
HELMEN Anders <anders.helmen@soprasteria.com>
2020-12-01 14:35:25 +0100

    Add utils to read from file

---
+++ utils.py
@@ -0,0 +1,8 @@
+import sys
+
+def inputfile_to_array(input_file):
+    with open(input_file) as f:
+        lines = f.read().splitlines()
+    lines_as_array = [int(line) for line in lines]
+    return lines_as_array
+
