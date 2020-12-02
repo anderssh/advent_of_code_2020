37c3cfc213ac8bf889112d73873d607fe9e15455
HELMEN Anders <anders.helmen@soprasteria.com>
2020-12-02 15:35:05 +0100

    add a prelim function to get inputs, not working as intended yet

--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import sys
+import requests
+import os.path
+
 
 def inputfile_to_array(input_file):
     with open(input_file) as f:
@@ -6,3 +9,11 @@ def inputfile_to_array(input_file):
     lines_as_array = [(line) for line in lines]
     return lines_as_array
 
+def get_input_file(day):
+
+    url = "https://adventofcode.com/2020/day/" + str(day) + "/input"
+    filepath = 'inputs/input_day_' + str(day) + ".txt"
+
+    if not os.path.isfile(filepath):
+        r = requests.get(url)
+        open(filepath, 'wb').write(r.content)
