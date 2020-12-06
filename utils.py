833a2227cd939ae3958ba10d56b64c9bed1e4357
HELMEN Anders <anders.helmen@soprasteria.com>
2020-12-06 21:29:56 +0100

    add 'remove_empty_lines_and_concat' to utils

--- utils.py
+++ utils.py
@@ -17,3 +17,26 @@ def get_input_file(day):
     if not os.path.isfile(filepath):
         r = requests.get(url)
         open(filepath, 'wb').write(r.content)
+
+def remove_empty_lines_and_concat(list_with_empty_lines, space_separated = True):
+    '''
+    Takes in the list as read from the file, puts the lines that belong together in
+    same list element.
+    '''
+    space_character = ''
+    if space_separated:
+        space_character = ' '
+    element = ''
+    line_list = []
+    for i, item in enumerate(list_with_empty_lines):
+        if item == "":
+            line_list.append(element)
+            element = ''
+        else:
+            if element == '':
+                element = element + item
+            else:
+                element = element + space_character + item
+            if i == len(list_with_empty_lines)-1:
+                line_list.append(element)
+    return line_list
