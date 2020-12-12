97ebe064b1c6e50799319c7c099ee0997f04b1d5
HELMEN Anders <anders.helmen@soprasteria.com>
2020-12-12 22:21:37 +0100

    still something wrong

--- day_11.py
+++ day_11.py
@@ -1,14 +1,9 @@
 from utils import inputfile_to_array
 
-seat_map = inputfile_to_array("inputs/input_day_11_test.txt")
-
-def print_map(seat_map):
-    for i in seat_map:
-        print(i)
+seat_map = inputfile_to_array("inputs/input_day_11.txt")
 
 def perform_seating(seat_map):
     out_map = seat_map[:]
-    print_map(out_map)
     for i in range(len(seat_map)):
         for j in range(len(seat_map[0])):
             adjacent_seats = get_adjacent_seats(seat_map,i,j)
@@ -16,13 +11,9 @@ def perform_seating(seat_map):
             if seat_map[i][j] == "L":
                 if num_ocupied_relevant_seats(seat_map, adjacent_seats) == 0:
                     out_map[i] = row_as_string[:j] + "#" + row_as_string[j + 1:]
-                    print(out_map[i])
             elif seat_map[i][j] == "#":
                 if num_ocupied_relevant_seats(seat_map, adjacent_seats) >= 4:
                     out_map[i] = row_as_string[:j] + "L" + row_as_string[j + 1:]
-                num_ocupied_relevant_seats(seat_map, adjacent_seats)
-    print("asdøflkajsdføalksjdfaølkdjfaølsdkfj")
-    print(out_map)
     return out_map
 
 def count_num_occupied_seats(seat_map):
@@ -35,19 +26,14 @@ def count_num_occupied_seats(seat_map):
 
 def num_ocupied_relevant_seats(seat_map, relevant_seats):
     occupied_counter = 0
-#    print("Relevant seats: ", relevant_seats)
     for seat in relevant_seats:
         seat_row,seat_column = seat
- #       print("Print av den som sjekkes", [seat_row, seat_column])
         if seat_map[seat_row][seat_column] == "#":
             occupied_counter += 1
-            print("OCU")
     return occupied_counter
 
 def get_adjacent_seats(seat_map, row_number, column_number):
     positions = []
-    #print("Current seat: ", seat_map[row_number][column_number])
-    #print("Current seat number: ", (row_number, column_number))
     if row_number == (len(seat_map)-1):
         positions.append((row_number-1,column_number))
         if column_number == (len(seat_map[0])-1):
@@ -55,7 +41,7 @@ def get_adjacent_seats(seat_map, row_number, column_number):
             positions.append((row_number,column_number-1))
         elif column_number == 0:
             positions.append((row_number-1,column_number+1))
-            positions.append((row_number,column_number))
+            positions.append((row_number,column_number+1))
         else:
             positions.append((row_number-1,column_number-1))
             positions.append((row_number-1,column_number+1))
@@ -64,7 +50,7 @@ def get_adjacent_seats(seat_map, row_number, column_number):
     elif row_number == (0):
         positions.append((row_number+1,column_number))
         if column_number == (len(seat_map[0])-1):
-            positions.append((row_number+1,column_number-1))
+            positions.append((row_number,column_number-1))
             positions.append((row_number,column_number-1))
         elif column_number == 0:
             positions.append((row_number,column_number+1))
@@ -75,12 +61,16 @@ def get_adjacent_seats(seat_map, row_number, column_number):
             positions.append((row_number+1,column_number-1))
             positions.append((row_number+1,column_number+1))
     else:
+        positions.append((row_number+1,column_number))
+        positions.append((row_number-1,column_number))
         if column_number == (len(seat_map[0])-1):
             positions.append((row_number+1,column_number-1))
+            positions.append((row_number-1,column_number-1))
             positions.append((row_number,column_number-1))
         elif column_number == 0:
             positions.append((row_number,column_number+1))
-            positions.append((row_number+1,column_number))
+            positions.append((row_number-1,column_number-1))
+            positions.append((row_number+1,column_number-1))
         else:
             positions.append((row_number,column_number-1))
             positions.append((row_number,column_number+1))
@@ -92,17 +82,18 @@ def get_adjacent_seats(seat_map, row_number, column_number):
             positions.append((row_number-1,column_number+1))
     return positions
 def get_last_seating(seat_map):
+    perform_seating_counter = 0
     old_seat_map = seat_map[:]
-
     while True:
-        new_seating = perform_seating(seat_map)
+        perform_seating_counter += 1
+        print("So many rounds in perform_seating:", perform_seating_counter)
+        new_seating = perform_seating(old_seat_map)
         print("The number of taken seats are:", count_num_occupied_seats(new_seating))
         if new_seating == old_seat_map:
             return new_seating
         old_seat_map = new_seating
 
 def task_1(seat_map):
-    print_map(seat_map)
     last_seating = get_last_seating(seat_map)
 
     return count_num_occupied_seats(last_seating)
